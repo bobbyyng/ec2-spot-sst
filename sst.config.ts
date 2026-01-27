78a4a00d79592851078305b0d657e2d58041d229
bobbyyng <httbrs116@gmail.com>
2026-01-27 22:44:28 +0800

    feat: limit the stage name

--- sst.config.ts
+++ sst.config.ts
@@ -3,13 +3,17 @@ export default $config({
   app(input) {
     return {
       name: "ec2-spot-sst",
-      removal: input?.stage === "production" ? "retain" : "remove",
-      protect: ["production"].includes(input?.stage),
+      removal: input?.stage === "prod" ? "retain" : "remove",
+      protect: ["prod", "dev"].includes(input?.stage),
       home: "aws",
       providers: { aws: "7.16.0" },
     };
   },
   async run() {
+    if (!["prod", "dev"].includes($app.stage)) {
+      return;
+    }
+
     const instanceInfra = await import("./infra/instance.infra")
 
     return {
